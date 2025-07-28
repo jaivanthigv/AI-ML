e5911ba7706f721bddac0af82f7864ef8a0db0fa
jaivanthigv <jaivanthigv1103@gmail.com>
2025-07-28 20:09:10 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,17 +3,16 @@ import streamlit as st
 from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 import matplotlib.pyplot as plt
+
 data = {
-'Outlook': ['Sunny', 'Sunny', 'Overcast', 'Rain', 'Rain', 'Rain', 'Overcast', 'Sunny',
-'Sunny', 'Rain', 'Sunny', 'Overcast', 'Overcast', 'Rain'],
-'Temperature':['Hot', 'Hot', 'Hot', 'Mild', 'Cool', 'Cool', 'Cool', 'Mild',
-'Cool', 'Mild', 'Mild', 'Mild', 'Hot', 'Mild'],
-'Humidity': ['High', 'High', 'High', 'High', 'Normal', 'Normal', 'Normal', 'High',
-'Normal', 'Normal', 'Normal', 'High', 'Normal', 'High'],
-'Wind': ['Weak', 'Strong', 'Weak', 'Weak', 'Weak', 'Strong', 'Strong', 'Weak',
-'Weak', 'Weak', 'Strong', 'Strong', 'Weak', 'Strong'],
-'PlayTennis': ['No', 'No', 'Yes', 'Yes', 'Yes', 'No', 'Yes', 'No',
-'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'No']
+
+    'Weather': ['Sunny', 'Rainy', 'Overcast', 'Sunny', 'Rainy', 'Sunny', 'Overcast', 'Rainy', 'Sunny', 'Rainy'],
+    'TimeOfDay': ['Morning', 'Morning', 'Afternoon', 'Afternoon', 'Evening', 'Morning', 'Morning', 'Afternoon',
+                  'Evening', 'Morning'],
+    'SleepQuality': ['Poor', 'Good', 'Poor', 'Good', 'Poor', 'Good', 'Poor', 'Good', 'Good', 'Poor'],
+    'Mood': ['Tired', 'Fresh', 'Tired', 'Energetic', 'Tired', 'Fresh', 'Tired', 'Tired', 'Energetic', 'Tired'],
+    'BuyCoffee': ['Yes', 'No', 'Yes', 'No', 'Yes', 'No', 'Yes', 'Yes', 'No', 'Yes']
+
 }
 df = pd.DataFrame(data)
 #Encode
@@ -26,8 +25,8 @@ for column in df.columns:
         label_encoder[column] = le
 
 #Feature and target
-X = df_encoded.drop("PlayTennis", axis = 1)
-y = df_encoded["PlayTennis"]
+X = df_encoded.drop("BuyCoffee", axis = 1)
+y = df_encoded["BuyCoffee"]
 
 #train decision tree
 
@@ -35,26 +34,30 @@ model = DecisionTreeClassifier(criterion = "entropy")
 model.fit(X,y)
 #Visualization with Matplotlib
 plt.figure(figsize = (12,6))
-plot_tree(model, feature_names = X.columns, class_names = label_encoder["PlayTennis"].classes_, filled= True)
+plot_tree(model, feature_names = X.columns, class_names = label_encoder["BuyCoffee"].classes_, filled= True)
 plt.show()
-st.title("PlayTennis Prediction with ID3 Decision Tree")
-# User input
-st.sidebar.header("Input Weather Conditions")
+st.title("BuyCoffee Prediction with ID3 Decision Tree")
+
+
 def user_input():
-    outlook = st.sidebar.selectbox("Outlook", df['Outlook'].unique())
-    temp = st.sidebar.selectbox("Temperature", df['Temperature'].unique())
-    humidity = st.sidebar.selectbox("Humidity", df['Humidity'].unique())
-    wind = st.sidebar.selectbox("Wind", df['Wind'].unique())
-    return pd.DataFrame([[outlook, temp, humidity, wind]],
-        columns=['Outlook', 'Temperature', 'Humidity', 'Wind'])
+    weather = st.sidebar.selectbox("Weather", df['Weather'].unique())
+    TimeOfDay = st.sidebar.selectbox("TimeOfDay", df['TimeOfDay'].unique())
+    SleepQuality = st.sidebar.selectbox("SleepQuality", df['SleepQuality'].unique())
+    Mood = st.sidebar.selectbox("Mood", df['Mood'].unique())
+
+    return pd.DataFrame([[weather, TimeOfDay, SleepQuality, Mood]],
+                        columns=['Weather', 'TimeOfDay', 'SleepQuality', 'Mood'])
+
+
 input_df = user_input()
+
 #Encode input
 input_encoded = input_df.copy()
 for col in input_encoded.columns:
     input_encoded[col] = label_encoder[col].transform(input_encoded[col])
 # Prediction
 prediction = model.predict(input_encoded)[0]
-prediction_label = label_encoder['PlayTennis'].inverse_transform([prediction])[0]
+prediction_label = label_encoder['BuyCoffee'].inverse_transform([prediction])[0]
 st.subheader("Prediction:")
 st.success(f"The model predicts: {prediction_label}")
 st.subheader("Input Values:")
